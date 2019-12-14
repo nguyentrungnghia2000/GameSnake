db4acfc87b58ea662fe5a36986ede613c96b02c5
nguyentrungnghia2000 <58599798+nguyentrungnghia2000@users.noreply.github.com>
2019-12-14 11:54:11 +0700

    Các Class

---
+++ GameSnake/GameSnake/Retangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSnake
+{
+    class Retangle
+    {
+        public Color color { get; }
+        public int X1 { get => X; set => X = value; }
+        public int Y1 { get => Y; set => Y = value; }
+        public int Width { get => width; set => width = value; }
+        public int Height { get => height; set => height = value; }
+
+        int X, Y;
+        int width, height;
+
+        public Retangle(int x, int y, int w, int h, Color c)
+        {
+            X1 = x; Y1 = y;
+            Width = w; Height = h;
+            color = c;
+        }
+
+        public Rectangle Rectangle()
+        {
+            return new Rectangle(X, Y, Width, Height);
+        }
+    }
+}
---
+++ GameSnake/GameSnake/Snake.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GameSnake
+{
+    class Snake
+    {
+        private Rectangle[] snakeRec;
+        public SolidBrush brush;
+        int x, y, w, h;
+        Random rand = new Random();
+
+        public Rectangle[] SnakeRec { get => snakeRec; set => snakeRec = value; }
+        #region DrawSnake
+        public Snake()
+        {
+            snakeRec = new Rectangle[3];
+            brush = new SolidBrush(Color.Purple);
+            w = 10; h = 10;
+            x = rand.Next(20, 550);
+            y = rand.Next(120, 380);
+            for (int i = 0; i < snakeRec.Length; i++)
+            {
+                snakeRec[i] = new Rectangle(x, y, w, h);
+                x -= 10;
+            }
+        }
+
+        public void DrawSnake(Graphics g)
+        {
+            foreach (Rectangle rec in SnakeRec)
+            {
+                g.FillRectangle(brush, rec);
+            }
+
+            g.FillRectangle(new SolidBrush(Color.Red), SnakeRec[0]);
+        }
+        #endregion
+
+        #region SnakeMove
+        public void SnakeRun()
+        {
+            for (int i = snakeRec.Length - 1; i > 0; i--)
+            {
+                snakeRec[i] = snakeRec[i - 1];
+            }
+        }
+
+        public void MoveDown()
+        {
+            SnakeRun();
+            snakeRec[0].Y += 10;
+        }
+
+        public void MoveUp()
+        {
+            SnakeRun();
+            snakeRec[0].Y -= 10;
+        }
+
+        public void MoveLeft()
+        {
+            SnakeRun();
+            snakeRec[0].X -= 10;
+        }
+
+        public void MoveRight()
+        {
+            SnakeRun();
+            snakeRec[0].X += 10;
+        }
+
+        #endregion
+
+        public void GrowSnake()
+        {
+            List<Rectangle> rec = snakeRec.ToList();
+            rec.Add(new Rectangle(snakeRec[snakeRec.Length - 1].X, snakeRec[snakeRec.Length - 1].Y, w, h));
+            snakeRec = rec.ToArray();
+        } // tự động lớn
+
+
+    }
+}
